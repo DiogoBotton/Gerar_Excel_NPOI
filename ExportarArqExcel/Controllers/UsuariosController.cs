9dfa1b53aeffa6c88967915944adca7eae7abb98
DiogoBotton <diogo.luo@gmail.com>
2020-07-02 09:54:16 -0300

    first commit

---
+++ ExportarArqExcel/Controllers/UsuariosController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ExportarArqExcel.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace ExportarArqExcel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        public IHostingEnvironment _hostingEnvironment;
+
+        public UsuariosController(IHostingEnvironment hosting)
+        {
+            _hostingEnvironment = hosting;
+        }
+
+        /// <summary>
+        /// Gerar Excel de Usuários
+        /// </summary>
+        /// <returns>Gera um arquivo .xlsx e disponibiliza o link para download do arquivo pelo navegador</returns>
+        [HttpGet("excel")]
+        public async Task<IActionResult> GerarExcelUsuarios()
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string nomeArquivo = "usuarios.xlsx";
+            try
+            {
+                if (Usuarios.listUsuarios.Count == 0)
+                {
+                    //Adiciona 5 usuários padrões
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Usuarios usuario = new Usuarios()
+                        {
+                            Nome = $"Usuario{i + 1}",
+                            Senha = "usuario123",
+                            Email = $"usuario{i + 1}@email.com",
+                            Cpf = "Não consta",
+                        };
+                        Usuarios.listUsuarios.Add(usuario);
+                    }
+                }
+
+                // Instancia classe que vai guardar o fluxo dos dados em memória
+                MemoryStream ms = new MemoryStream();
+
+                //Using para criação da tabela
+                using (FileStream fs = new FileStream(Path.Combine(webRootPath, nomeArquivo), FileMode.Create))
+                {
+
+                    // Cria arquivo excel .xlsx (com .xls ocorrem problemas)
+                    IWorkbook workbook = new XSSFWorkbook();
+                    // Cria um novo plano para o Excel
+                    ISheet sheet = workbook.CreateSheet("Usuarios");
+
+                    int rowNumber = 0;
+
+                    // Cria uma nova linha (primeira linha)
+                    IRow row = sheet.CreateRow(rowNumber);
+                    ICell cell;
+
+                    // Cria uma celula na linha, em seguida, coloca o valor da célula
+                    cell = row.CreateCell(0);
+                    cell.SetCellValue("Nome");
+
+                    cell = row.CreateCell(1);
+                    cell.SetCellValue("Email");
+
+                    cell = row.CreateCell(2);
+                    cell.SetCellValue("CPF");
+
+                    rowNumber++;
+
+                    // For para implementação de todos os usuarios da lista
+                    for (int i = 0; i < Usuarios.listUsuarios.Count; i++)
+                    {
+                        // Cria uma nova linha para implementação de novo usuário
+                        row = sheet.CreateRow(rowNumber);
+
+                        row.CreateCell(0).SetCellValue(Usuarios.listUsuarios[i].Nome);
+                        row.CreateCell(1).SetCellValue(Usuarios.listUsuarios[i].Email);
+                        row.CreateCell(2).SetCellValue(Usuarios.listUsuarios[i].Cpf);
+
+                        rowNumber++;
+                    }
+
+                    //Tamanho das colunas
+                    sheet.SetColumnWidth(0, 40 * 256);
+                    sheet.SetColumnWidth(1, 40 * 256);
+                    sheet.SetColumnWidth(2, 40 * 256);
+
+                    workbook.Write(fs);
+                }
+
+                // Using responsável para gerar o arquivo na pasta wwwroot
+                using (FileStream fs = new FileStream(Path.Combine(webRootPath, nomeArquivo), FileMode.Open))
+                {
+                    await fs.CopyToAsync(ms);
+                }
+
+                ms.Position = 0;
+                // Método herdado de BaseController que recebe a memoryStream, ContentType(diz que é um arquivo .xlsx) e o nome do arquivo.
+                var retorno = File(ms, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", nomeArquivo);
+
+                return Ok(retorno);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, $"Ocorreu um erro na geração do Excel de Usuários / {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ExportarArqExcel/Models/Usuarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExportarArqExcel.Models
+{
+    public class Usuarios
+    {
+        public static List<Usuarios> listUsuarios = new List<Usuarios>();
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Cpf { get; set; }
+
+        public Usuarios()
+        {
+
+        }
+
+        public Usuarios(string nome, string email, string senha, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Cpf = cpf;
+        }
+    }
+}
